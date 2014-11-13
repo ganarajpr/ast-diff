42dcccc6169d4d33aa559c7c2ed5c1134b6bdfc6
ganara.jpermunda <ganaraj.permunda@lab49.com>
2014-11-13 13:29:36 +0000

    listing matches.

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ var nodeheight = require('./lib/nodeheight');
 var Matcher = require('./lib/TopDownMatcher');
 
 module.exports = function(src,dest){
-    Matcher.match(src,dest);
+    var mapped = Matcher.match(src,dest);
+    mapped.map(function(pair){
+        console.log(pair.src.type);
+        console.log("source loc", pair.src.loc.start);
+        console.log("dest loc" , pair.dst.loc.start);
+    });
 };
